1746ae7e43ee2de476cd5b9183bff2a442f93c7f
saraclsimoes <60150284+saraclsimoes@users.noreply.github.com>
2020-02-26 19:23:02 +0000

    Add HTML, JS and CSS files

---
+++ script.js
@@ -0,0 +1,25 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  moscow: {
+    temp: -5,
+    humidity: 20
+  }
+};
+let city = prompt("Enter City");
+city.toLowerCase();
+if (city === weather[0]) alert("It is currently");
